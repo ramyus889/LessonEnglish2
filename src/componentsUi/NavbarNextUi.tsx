ca72a4ecfba20cad9a1748c057c44054912b7621
ramyus889 <ramyus889.com>
2024-04-05 18:30:05 +0300

    first

--- src/componentsUi/NavbarNextUi.tsx
+++ src/componentsUi/NavbarNextUi.tsx
@@ -17,10 +17,22 @@ export default function App() {
   return (
     <Navbar shouldHideOnScroll>
       <NavbarBrand>
-        <AcmeLogo />
-        <Link className="pe-5" href="/">
-          Frontend
-        </Link>
+        <div className="max-[640px]:flex  max-[640px]:gap-5 max-[640px]:mt-20 max-[640px]:items-center">
+        <div className="flex">
+          <AcmeLogo />  
+          <Link className="pe-5" href="/">
+            Frontend
+          </Link>
+        </div>
+          <Link
+            className={`px-5 py-2 border-2 text-center my-5 flex border-slate-500 rounded-xl min-[640px]:hidden ${
+              pathname === "/Lesson" ? "hidden" : ""
+            }`}
+            href="/Lesson"
+          >
+            Back
+          </Link>
+        </div>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
