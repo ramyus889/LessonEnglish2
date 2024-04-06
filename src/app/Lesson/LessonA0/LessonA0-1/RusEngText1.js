f57714235fc89d4800b0c245c6ea37fa1c342701
ramyus889 <ramyus889.com>
2024-04-06 19:47:12 +0300

    first

---
+++ src/app/Lesson/LessonA0/LessonA0-1/RusEngText1.js
@@ -0,0 +1,206 @@
+export const rusEngText1 = [
+  {
+    engText: "I work in a company",
+    rusText: " Я работаю в компании",
+  },
+  {
+    engText: "company",
+    rusText: "компания",
+  },
+  {
+    engText: "food",
+    rusText: "еда",
+  },
+  {
+    engText: "He loves",
+    rusText: "Он любит",
+  },
+  {
+    engText: "hotel",
+    rusText: "отель",
+  },
+  {
+    engText: "keys",
+    rusText: "ключи",
+  },
+  {
+    engText: "I agree",
+    rusText: "Я согласен",
+  },
+  {
+    engText: "I understand",
+    rusText: "Я понимаю",
+  },
+  {
+    engText: "I know",
+    rusText: "Я знаю",
+  },
+  {
+    engText: "We support you",
+    rusText: "Мы поддерживаем тебя",
+  },
+  {
+    engText: "You do it very well",
+    rusText: "Ты делаешь это очень хорошо",
+  },
+  {
+    engText: " They want more",
+    rusText: "Они хотят больше",
+  },
+  {
+    engText: "I agree with you",
+    rusText: "Я согласен с тобой",
+  },
+  {
+    engText: "I support you",
+    rusText: "Я поддерживаю тебя",
+  },
+  {
+    engText: "We see this problem",
+    rusText: "Мы видим эту проблему",
+  },
+  {
+    engText: "They work in this place",
+    rusText: " Они работают в этом месте",
+  },
+  {
+    engText: "I have a friend",
+    rusText: "У меня есть друг",
+  },
+  {
+    engText: "I have a question",
+    rusText: "У меня вопрос",
+  },
+  {
+    engText: "I have a ticket",
+    rusText: "У меня есть билет",
+  },
+  {
+    engText: "I have an idea",
+    rusText: "У меня есть идея",
+  },
+  {
+    engText: "I have an umbrella",
+    rusText: "У меня есть зонт",
+  },
+  {
+    engText: "I see a university",
+    rusText: "Я вижу университет",
+  },
+  {
+    engText: "We see an airport",
+    rusText: "Мы видим аэропорт",
+  },
+  {
+    engText: "I see a supermarket",
+    rusText: "Я вижу супермаркет",
+  },
+  {
+    engText: "I see a hotel",
+    rusText: "Я вижу отель",
+  },
+  {
+    engText: "I have a problem",
+    rusText: "У меня проблема",
+  },
+  {
+    engText: "I like English",
+    rusText: "Мне нравится английский",
+  },
+  {
+    engText: "I like this place",
+    rusText: "Мне нравится это место",
+  },
+  {
+    engText: " I like this food",
+    rusText: " Мне нравится эта еда",
+  },
+  {
+    engText: "I see a train",
+    rusText: "Я вижу поезд",
+  },
+  {
+    engText: "I see a bus",
+    rusText: "Я вижу автобус",
+  },
+  {
+    engText: "I have a result",
+    rusText: "У меня есть результат",
+  },
+  {
+    engText: "I have a family",
+    rusText: "У меня есть семья",
+  },
+  {
+    engText: "I have a dream",
+    rusText: "У меня есть мечта",
+  },
+  {
+    engText: "I have an aim",
+    rusText: " У меня есть цель",
+  },
+  {
+    engText: "They have a dream",
+    rusText: "У них есть мечта",
+  },
+  {
+    engText: "We have an aim",
+    rusText: "У нас есть цель",
+  },
+  {
+    engText: "You have a talent",
+    rusText: "У тебя есть талант",
+  },
+  {
+    engText: "I work in a company",
+    rusText: "Я работаю в компании",
+  },
+  {
+    engText: "They work in a company",
+    rusText: "Они работают в компании",
+  },
+  {
+    engText: "I work in this company",
+    rusText: " Я работаю в этой компании",
+  },
+  {
+    engText: " I see a message",
+    rusText: " Я вижу сообщение",
+  },
+  {
+    engText: "We see a message ",
+    rusText: " Мы видим сообщение",
+  },
+  {
+    engText: "I have a job",
+    rusText: "У меня есть работа",
+  },
+  {
+    engText: "They have a job",
+    rusText: " У них есть работа",
+  },
+  {
+    engText: "I really want it ",
+    rusText: " Я очень хочу это",
+  },
+  {
+    engText: "I really like it",
+    rusText: "Мне очень нравится это",
+  },
+  {
+    engText: "I really like this country ",
+    rusText: "Мне очень нравится эта страна",
+  },
+  {
+    engText: "I really like this city",
+    rusText: "Мне очень нравится этот город",
+  },
+  {
+    engText: "I really like this course",
+    rusText: " Мне очень нравится этот курс",
+  },
+  {
+    engText: "I really like English",
+    rusText: "Мне очень нравится английский",
+  },
+];
