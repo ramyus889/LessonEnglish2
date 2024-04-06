ec41fcd6999d8d6c207084448cf9a176fb85132e
ramyus889 <ramyus889.com>
2024-04-06 19:31:59 +0300

    first

---
+++ src/app/Lesson/LessonA0/LessonA0-2/RusEngText2.js
@@ -0,0 +1,186 @@
+export const rusEngText2 = [
+  {
+    engText: "I work in a company",
+    rusText: "Я работаю в компании",
+  },
+  {
+    engText: "I like English ",
+    rusText: " Мне нравится английский",
+  },
+  {
+    engText: "You do it very well",
+    rusText: "Вы делаете это очень хорошо",
+  },
+  {
+    engText: "They want more",
+    rusText: "Они хотят больше",
+  },
+  {
+    engText: "We have a job",
+    rusText: "У нас есть работа",
+  },
+  {
+    engText: "I have a dream",
+    rusText: "У меня есть мечта",
+  },
+  {
+    engText: "I see a supermaket",
+    rusText: "Я вижу супермаркет",
+  },
+  {
+    engText: "I see a hotel",
+    rusText: "Я вижу отель",
+  },
+  {
+    engText: "I really like it",
+    rusText: "Мне очень нравится это",
+  },
+  {
+    engText: "We see a university",
+    rusText: "Мы видим университет",
+  },
+  {
+    engText: "We see this problem",
+    rusText: "Мы видим эту проблему",
+  },
+  {
+    engText: "We see a message",
+    rusText: "Мы видим сообщение",
+  },
+  {
+    engText: "I work in this company",
+    rusText: "Я работаю в этой компании",
+  },
+  {
+    engText: "I agree with you",
+    rusText: " Я согласен с тобой",
+  },
+  {
+    engText: "I have an umbrella",
+    rusText: "У меня есть зонт",
+  },
+  {
+    engText: "I like this food ",
+    rusText: "Мне нравится эта еда",
+  },
+  {
+    engText: "I have a job",
+    rusText: "У меня есть работа",
+  },
+  {
+    engText: "I have a friend",
+    rusText: "У меня есть друг",
+  },
+  {
+    engText: "I really want it",
+    rusText: "Я очень хочу это",
+  },
+  {
+    engText: "I like this place",
+    rusText: "Мне нравится это место",
+  },
+  {
+    engText: "I see a bus",
+    rusText: "Я вижу автобус",
+  },
+  {
+    engText: "They have a job",
+    rusText: "У них есть работа",
+  },
+  {
+    engText: "I have a family",
+    rusText: "У меня есть семья",
+  },
+  {
+    engText: "I support you",
+    rusText: "Я поддерживаю тебя",
+  },
+  {
+    engText: "We support you",
+    rusText: "Мы поддерживаем тебя",
+  },
+  {
+    engText: "I agree",
+    rusText: "Я согласен",
+  },
+  {
+    engText: "I have an aim",
+    rusText: "У меня есть цель",
+  },
+  {
+    engText: "I have a problem",
+    rusText: "У меня проблема",
+  },
+  {
+    engText: "I have a question",
+    rusText: "У меня вопрос",
+  },
+  {
+    engText: "We have an aim",
+    rusText: "У нас есть цель",
+  },
+  {
+    engText: "They have a dream",
+    rusText: "У них есть мечта",
+  },
+  {
+    engText: "I really like this city",
+    rusText: "Мне очень нравится этот город",
+  },
+  {
+    engText: "I know",
+    rusText: "Я знаю",
+  },
+  {
+    engText: "I see a train",
+    rusText: "Я вижу поезд",
+  },
+  {
+    engText: "I have an idea",
+    rusText: " У меня есть идея",
+  },
+  {
+    engText: "They work in a company",
+    rusText: "Они работают в компании",
+  },
+  {
+    engText: "We have a dream",
+    rusText: "У нас есть мечта",
+  },
+  {
+    engText: "You have a talent",
+    rusText: "У вас есть талант",
+  },
+  {
+    engText: "They have an idea",
+    rusText: "У них есть идея",
+  },
+  {
+    engText: "I really like this course",
+    rusText: "Мне очень нравится этот курс",
+  },
+  {
+    engText: "I see an airport",
+    rusText: "Я вижу аэропорт",
+  },
+  {
+    engText: " They work in this place",
+    rusText: " Они работают в этом месте",
+  },
+  {
+    engText: "I see a university",
+    rusText: "Я вижу университет",
+  },
+  {
+    engText: "I see a message",
+    rusText: "Я вижу сообщение",
+  },
+  {
+    engText: "I have a ticket",
+    rusText: "У меня есть билет",
+  },
+  {
+    engText: "I understand ",
+    rusText: "Я понимаю",
+  },
+];
---
+++ src/app/Lesson/LessonA0/LessonA0-3/RusEngText3.js
@@ -0,0 +1,206 @@
+export const rusEngText3 = [
+  {
+    engText: "I often go for a walk",
+    rusText: "Я часто хожу на прогулку",
+  },
+  {
+    engText: "I want to go for a walk",
+    rusText: "Я хочу пойти на прогулку",
+  },
+  {
+    engText: "I want to buy it",
+    rusText: "Я хочу купить это",
+  },
+  {
+    engText: "I want to buy this ticket",
+    rusText: "Я хочу купить этот билет",
+  },
+  {
+    engText: "I want to buy these tickets",
+    rusText: " Я хочу купить эти билеты",
+  },
+  {
+    engText: "I want to order it online",
+    rusText: "Я хочу заказать это онлайн",
+  },
+  {
+    engText: "I want to order this food online",
+    rusText: "Я хочу заказать эту еду онлайн",
+  },
+  {
+    engText: "I want to order these tickets online",
+    rusText: "Я хочу заказать эти билеты онлайн",
+  },
+  {
+    engText: "I want to visit England",
+    rusText: "Я хочу посетить Англию",
+  },
+  {
+    engText: "I want to visit Spain",
+    rusText: "Я хочу посетить Испанию",
+  },
+  {
+    engText: "I'd like to visit Kaiakent",
+    rusText: "Я бы хотел посетить Каякент",
+  },
+  {
+    engText: "I'd like to visit this country",
+    rusText: "Я бы хотел посетить эту страну",
+  },
+  {
+    engText: "I'd like to visit this city",
+    rusText: "Я бы хотел посетить этот город",
+  },
+  {
+    engText: "I want to visit new countries and cities",
+    rusText: "Я хочу посетить новые страны и города",
+  },
+  {
+    engText: "I'd like to drink tea",
+    rusText: "Я хотел бы попить чаю",
+  },
+  {
+    engText: "I'd like to drink coffee",
+    rusText: "Я хотел бы попить кофе",
+  },
+  {
+    engText: "I'd like to try on this dress",
+    rusText: "Я бы хотела примерить это платье",
+  },
+  {
+    engText: "I'd like to try on this sweater",
+    rusText: " Я бы хотел примерить этот свитер",
+  },
+  {
+    engText: "I'd like to try on these shoes",
+    rusText: "Я бы хотел примерить эту обувь",
+  },
+  {
+    engText: "I'd like to try on these jeans",
+    rusText: " Я бы хотел примерить эти джинсы",
+  },
+  {
+    engText: "I'd like to try on these trousers",
+    rusText: "Я бы хотел примерить эти брюки",
+  },
+  {
+    engText: " I want to learn English",
+    rusText: "Я хочу выучить английский",
+  },
+  {
+    engText: "I want to call you now",
+    rusText: "Я хочу позвонить тебе сейчас",
+  },
+  {
+    engText: "I want to go to this shop",
+    rusText: " Я хочу пойти в этот магазин",
+  },
+  {
+    engText: "I want to go to this restaurant",
+    rusText: "Я хочу пойти в этот ресторан",
+  },
+  {
+    engText: "I want to go there with you",
+    rusText: "Я хочу пойти туда с тобой",
+  },
+  {
+    engText: "I want to talk about it",
+    rusText: "Я хочу поговорить об этом",
+  },
+  {
+    engText: "I'd like to talk about it now",
+    rusText: "Я бы хотел поговорить об этом сейчас",
+  },
+  {
+    engText: "I want to say one thing",
+    rusText: "Я хочу сказать одну вещь",
+  },
+  {
+    engText: "I want to tell you one thing",
+    rusText: "Я хочу сказать тебе одну вещь",
+  },
+  {
+    engText: "I want to say one very important thing",
+    rusText: "Я хочу сказать очень важную вещь",
+  },
+  {
+    engText: "I want to tell you about one problem",
+    rusText: "Я хочу сказать тебе об одной проблеме",
+  },
+  {
+    engText: "I need to do it",
+    rusText: "Мне нужно сделать это",
+  },
+  {
+    engText: "I need to do it now",
+    rusText: " Мне нужно сделать это сейчас",
+  },
+  {
+    engText: "I need to go home ",
+    rusText: "Мне нужно идти домой",
+  },
+  {
+    engText: "I'd like to go home",
+    rusText: "Я бы хотел пойти домой",
+  },
+  {
+    engText: "I need to go to work",
+    rusText: "Мне нужно идти на работу",
+  },
+  {
+    engText: "I need to be there soon",
+    rusText: "Мне нужно быть там скоро",
+  },
+  {
+    engText: "I go to work ",
+    rusText: "Я хожу на работу",
+  },
+  {
+    engText: "They go to school",
+    rusText: "Они ходят в школу",
+  },
+  {
+    engText: "We go to university",
+    rusText: "Мы ходим в университет",
+  },
+  {
+    engText: "I often go there",
+    rusText: "Я часто хожу туда",
+  },
+  {
+    engText: "I want to go to the park",
+    rusText: "Я хочу пойти в парк",
+  },
+  {
+    engText: "I'd like to go to the park with you",
+    rusText: "Я бы хотел пойти в парк с тобой",
+  },
+  {
+    engText: "I'd like to go to Kayakent",
+    rusText: "Я бы хотел поехать в Каякент",
+  },
+  {
+    engText: "I want to go to Dagestan ",
+    rusText: "Я хочу поехать в Дагестан",
+  },
+  {
+    engText: "I'd like to go to Dagestan next year",
+    rusText: " Я бы хотел поехать в Дагестан в следующем году",
+  },
+  {
+    engText: "I want to go to Kayakent",
+    rusText: "Я хочу поехать в Каякент",
+  },
+  {
+    engText: "I'd like to go to that place ",
+    rusText: " Я бы хотел пойти в это место",
+  },
+  {
+    engText: "I'd like to go for a walk now",
+    rusText: "Я бы хотел пойти на прогулку сейчас",
+  },
+  {
+    engText: "I'd like to visit new countries and cities",
+    rusText: "Я бы хотел посетить новые страны и города",
+  },
+];
