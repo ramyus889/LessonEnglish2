6e2505f9811ef2387475b4e836eb7d6f16459cdc
ramyus889 <ramyus889.com>
2024-04-18 10:17:09 +0300

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ import Card from "./components/Card";
 import BigCard from "./components/BigCard";
 import ThreeCard from "./components/ThreeCard";
 import Footer from "./components/Footer";
+import Pin3dAcert from "./../components/Pin3dAcert";
 
 export default function Home() {
   return (
     <div className="">
       <Head />
       <Card />
+      <Pin3dAcert />
       <BigCard />
       <ThreeCard />
       <Footer />
---
+++ src/components/Pin3dAcert.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import { PinContainer } from "./ui/3d-pin";
+import Image from "next/image";
+import imgFront from "/public/frontend.jpg";
+
+export default function AnimatedPinDemo() {
+  return (
+    <div className="h-[40rem] w-full px-5 flex items-center justify-center ">
+      <PinContainer title="/ui.aceternity.com" href="/Lessons">
+        <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+          <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+            Frontend Ui
+          </h3>
+          <div className="text-base !m-0 !p-0 font-normal">
+            <span className="text-slate-500 ">
+              Customizable Tailwind CSS and Framer Motion Components.
+            </span>
+          </div>
+          <Image
+            src={imgFront}
+            alt="pin"
+            className=" pb-5 px-5"
+            styles={{ width: "auto", height: "auto" }}
+          />
+        </div>
+      </PinContainer>
+    </div>
+  );
+}
