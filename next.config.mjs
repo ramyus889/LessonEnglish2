70e233c71e046cd338f7212f88e3e90c4f4c6f46
ramyus889 <ramyus889.com>
2024-04-24 14:50:25 +0300

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: [
+      "@emotion/react",
+      "@emotion/styled",
+      "@hookform/resolvers",
+      "@internationalized/date",
+      "@mantine/core",
+      "@nextui-org/react",
+      "@react-three/fiber",
+      "@tabler/icons-react",
+      "aos",
+      "class-variance-authority",
+      "clsx",
+      "cmdk",
+      "date-fns",
+      "embla-carousel-react",
+      "framer-motion",
+      "gsap",
+      "simplex-noise",
+      "tailwind-merge",
+      "tailwindcss-animate",
+      "three",
+      "transition-style",
+    ],
+  },
+};
 
 export default nextConfig;
