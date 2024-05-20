13b0a54f41ab162c2d6df157930d3a6b0e0f262c
ramyus889 <ramyus889.com>
2024-05-20 17:58:53 +0300

    first

--- src/app/Lessons/LessonA0/LessonA0-6/RusEngText6.js
+++ src/app/Lessons/LessonA0/LessonA0-6/RusEngText6.js
@@ -1,12 +1,287 @@
 export const rusEngText6 = [
   {
-    id: "5792",
+    id: "27862",
+    engText: "She travels every summer",
+    rusText: "Она путешествует каждое лето",
+  },
+  {
+    id: "86358",
+    engText: "She likes this food",
+    rusText: "Ей нравится эта еда",
+  },
+  {
+    id: "17450",
+    engText: "She lives in the United States",
+    rusText: "Она живет в США",
+  },
+  {
+    id: "37326",
+    engText: "She has a brother",
+    rusText: "У нее есть брат",
+  },
+  {
+    id: "48022",
+    engText: "He needs your help",
+    rusText: "Ему нужна твоя помощь",
+  },
+  {
+    id: "3845",
+    engText: "He sometimes tells me about it",
+    rusText: "Он иногда говорит мне об этом",
+  },
+  {
+    id: "83971",
+    engText: "She works in company",
+    rusText: "Она работает в компании",
+  },
+  {
+    id: "31903",
+    engText: "He has a car",
+    rusText: "У него есть машина",
+  },
+  {
+    id: "43140",
+    engText: "She has so many questions",
+    rusText: "У нее очень много вопросов",
+  },
+  {
+    id: "86667",
+    engText: "It costs so much money",
+    rusText: "Это стоит так много денег",
+  },
+  {
+    id: "23321",
+    engText: "He has a dog",
+    rusText: "У него есть собака",
+  },
+  {
+    id: "778",
+    engText: "He looks very young",
+    rusText: "Он выглядит очень молодым",
+  },
+  {
+    id: "9977",
+    engText: "It helps me so much",
+    rusText: "Это помогает мне очень сильно",
+  },
+  {
+    id: "42052",
+    engText: "She likes this style",
+    rusText: "Ей нравится этот стиль",
+  },
+  {
+    id: "91381",
+    engText: "He needs more money",
+    rusText: "Ему нужно больше денег",
+  },
+  {
+    id: "22211",
+    engText: "She sometimes says it",
+    rusText: "Она иногда говорит это",
+  },
+  {
+    id: "31321",
+    engText: "She likes this song",
+    rusText: "Ей нравится эта песня",
+  },
+  {
+    id: "2835",
+    engText: "She needs your advice",
+    rusText: "Ей нужен твой совет",
+  },
+  {
+    id: "42470",
+    engText: "She lives in this area",
+    rusText: "Она живет в этом районе",
+  },
+  {
+    id: "64490",
+    engText: "He often travels",
+    rusText: "Он часто путешествует",
+  },
+  {
+    id: "52332",
+    engText: "It costs twenty dollars",
+    rusText: "Это стоит двадцать долларов",
+  },
+  {
+    id: "20395",
+    engText: "He has a question",
+    rusText: "У него есть вопрос",
+  },
+  {
+    id: "99482",
+    engText: "He does it much better",
+    rusText: "Он делает это намного лучше",
+  },
+  {
+    id: "98820",
+    engText: "It costs two hundred dollars",
+    rusText: "Это стоит двести долларов",
+  },
+  {
+    id: "64727",
+    engText: "He drives a car",
+    rusText: "Он водит машину",
+  },
+  {
+    id: "6292",
+    engText: "It seems strange",
+    rusText: "Это кажется странным",
+  },
+  {
+    id: "23197",
+    engText: "She does it very well",
+    rusText: "Она делает это очень хорошо",
+  },
+  {
+    id: "3397",
+    engText: "He has too little free time",
+    rusText: "У него слишком мало свободного времени",
+  },
+  {
+    id: "59691",
+    engText: "She has a problem",
+    rusText: "У нее проблема",
+  },
+  {
+    id: "51400",
+    engText: "She often tells me about it",
+    rusText: "Она часто говорит мне об этом",
+  },
+  {
+    id: "17528",
+    engText: "It looks very beautiful",
+    rusText: "Это выглядит очень красиво",
+  },
+  {
+    id: "68490",
+    engText: "This bag costs thirty dollars",
+    rusText: "Эта сумка стоит тридцать долларов",
+  },
+  {
+    id: "9991",
+    engText: "He lives in this area",
+    rusText: "Он живет в этом районе",
+  },
+  {
+    id: "67817",
+    engText: "This ticket costs forty dollars",
+    rusText: "Этот билет стоит сорок долларов",
+  },
+  {
+    id: "13521",
+    engText: "She has a cat",
+    rusText: "У нее есть кошка",
+  },
+  {
+    id: "36376",
+    engText: "He often says it",
+    rusText: "Он часто говорит это",
+  },
+  {
+    id: "57486",
+    engText: "He likes this design",
+    rusText: "Ему нравится этот дизайн",
+  },
+  {
+    id: "59950",
+    engText: "He likes this music",
+    rusText: "Ему нравится эта музыка",
+  },
+  {
+    id: "44286",
+    engText: "It seems really strange",
+    rusText: "Это кажется действительно странным",
+  },
+  {
+    id: "14306",
+    engText: "He lives  in London",
+    rusText: "Он живет в Лондоне",
+  },
+  {
+    id: "47288",
+    engText: "It really helps me",
+    rusText: "Это действительно помогает мне",
+  },
+  {
+    id: "46911",
+    engText: "She needs more time",
+    rusText: "Ей нужно больше времени",
+  },
+  {
+    id: "91877",
+    engText: "This ticket costs three hundred dollars ",
+    rusText: "Этот билет стоит триста долларов",
+  },
+  {
+    id: "98638",
+    engText: "He has a sister",
+    rusText: "У него есть сестра",
+  },
+  {
+    id: "13310",
+    engText: "He works in a bank",
+    rusText: "Он работает в банке",
+  },
+  {
+    id: "70926",
+    engText: "It costs ten euros ",
+    rusText: "Это стоит десять евро",
+  },
+  {
+    id: "32038",
+    engText: "This computer costs two thousand dollars ",
+    rusText: "Этот компьютер стоит две тысячи долларов",
+  },
+  {
+    id: "87474",
+    engText: "This model costs three thousand dollars",
+    rusText: "Эта модель стоит три тысячи долларов",
+  },
+  {
+    id: "54662",
+    engText: "It costs fifty pounds ",
+    rusText: "Это стоит пятьдесят фунтов",
+  },
+  {
+    id: "94302",
+    engText: "He likes this program",
+    rusText: "Ему нравится эта программа",
+  },
+  {
+    id: "22792",
     engText: "He loves her",
     rusText: "Он любит ее",
   },
   {
-    id: "6208",
+    id: "21724",
+    engText: "He often laughs",
+    rusText: "Он часто смеется",
+  },
+  {
+    id: "2629",
+    engText: "She studies  there",
+    rusText: "Она учится там",
+  },
+  {
+    id: "7307",
     engText: "She plays there very often",
-    rusText: "Она очень часто там играет",
+    rusText: "Она играет там очень часто",
+  },
+  {
+    id: "2172422",
+    engText: "He teaches  English",
+    rusText: "Он преподает английский",
+  },
+  {
+    id: "262933",
+    engText: "She often laughs",
+    rusText: "Она часто смеется",
+  },
+  {
+    id: "730744",
+    engText: "She watches  these video",
+    rusText: "Она смотрит эти видео уроки",
   },
 ];
