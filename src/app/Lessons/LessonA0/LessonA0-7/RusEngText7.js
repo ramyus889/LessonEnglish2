fa96189c1ddde6ac6f9cffef38e7ed0eeed214e8
ramyus889 <ramyus889.com>
2024-05-21 11:15:41 +0300

    first

---
+++ src/app/Lessons/LessonA0/LessonA0-7/RusEngText7.js
@@ -0,0 +1,247 @@
+export const rusEngText7 = [
+  {
+    id: "3284",
+    engText: "I don't like this food",
+    rusText: "Мне не нравится эта еда",
+  },
+  {
+    id: "4515",
+    engText: "I don't want to go there now",
+    rusText: "Я не хочу идти туда сейчас",
+  },
+  {
+    id: "3587",
+    engText: "We don't see any changes",
+    rusText: "Мы не видим каких-либо изменений",
+  },
+  {
+    id: "5089",
+    engText: "They don't want to say it",
+    rusText: "Они не хотят говорить это",
+  },
+  {
+    id: "8146",
+    engText: "I see you don't want it",
+    rusText: "Я вижу, ты не хочешь это",
+  },
+  {
+    id: "6807",
+    engText: "He doesn't want to talk about it",
+    rusText: "Он не хочет говорить об этом",
+  },
+  {
+    id: "3004",
+    engText: "She doesn't work so much",
+    rusText: "Она не работает так много",
+  },
+  {
+    id: "7153",
+    engText: "It doesn't help me",
+    rusText: "Это не помогает мне",
+  },
+  {
+    id: "5675",
+    engText: "It doesn't work",
+    rusText: "Это не работает",
+  },
+  {
+    id: "7534",
+    engText: "I see that it doesn't work",
+    rusText: "Я вижу, что это не работает",
+  },
+  {
+    id: "9846",
+    engText: "I see that it doesn't help me",
+    rusText: "Я вижу, что это не помогает мне",
+  },
+  {
+    id: "9562",
+    engText: "He doesn't speak English",
+    rusText: "Он не говорит по-английски",
+  },
+  {
+    id: "2139",
+    engText: "She doesn't understand it",
+    rusText: "Она не понимает это",
+  },
+  {
+    id: "3255",
+    engText: "I don't have much free time",
+    rusText: "У меня нет много свободного Времени",
+  },
+  {
+    id: "2224",
+    engText: "I don't travel so often",
+    rusText: "Я не путешествую так часто",
+  },
+  {
+    id: "3336",
+    engText: "I don't know the answer",
+    rusText: "Я не знаю ответа",
+  },
+  {
+    id: "5430",
+    engText: "He doesn't know the answer",
+    rusText: "Он не знает ответа",
+  },
+  {
+    id: "7911",
+    engText: "She doesn't know the answer",
+    rusText: "Она не знает ответа",
+  },
+  {
+    id: "6790",
+    engText: "It doesn't cost this money",
+    rusText: "Это не стоит этих денег",
+  },
+  {
+    id: "6700",
+    engText: "He doesn't have a car",
+    rusText: "У него нет машины",
+  },
+  {
+    id: "8740",
+    engText: "She doesn't want to do it",
+    rusText: "Она не хочет делать это",
+  },
+  {
+    id: "4260",
+    engText: "He doesn't like this idea",
+    rusText: "Ему не нравится эта идея",
+  },
+  {
+    id: "4373",
+    engText: "She doesn't like this restaurant",
+    rusText: "Ей не нравится этот ресторан",
+  },
+  {
+    id: "9469",
+    engText: "He doesn't like this song",
+    rusText: "Ему не нравится эта песня",
+  },
+  {
+    id: "5218",
+    engText: "She doesn't like this shop",
+    rusText: "Ей не нравится этот магазин",
+  },
+  {
+    id: "2824",
+    engText: "I don't think about it",
+    rusText: "Я не думаю об этом",
+  },
+  {
+    id: "3636",
+    engText: "I don't think so",
+    rusText: "Я так не думаю",
+  },
+  {
+    id: "5995",
+    engText: "I don't agree with it",
+    rusText: "Я не согласен с этим",
+  },
+  {
+    id: "2199",
+    engText: "He doesn't think about it",
+    rusText: "Он не думает об этом",
+  },
+  {
+    id: "6418",
+    engText: "She doesn't speak English",
+    rusText: "Она не говорит по-английски",
+  },
+  {
+    id: "7012",
+    engText: "He doesn't say it",
+    rusText: "Он не говорит эту вещь",
+  },
+  {
+    id: "5569",
+    engText: "He doesn't tell me this thing",
+    rusText: "Он не говорит мне эту вещь",
+  },
+  {
+    id: "7455",
+    engText: "She doesn't tell me about it",
+    rusText: "Она не говорит мне об этом",
+  },
+  {
+    id: "4899",
+    engText: "I don't ask him about it",
+    rusText: "Я не спрашиваю его об этом",
+  },
+  {
+    id: "8713",
+    engText: "He doesn't feel happy",
+    rusText: "Он не чувствует себя счастливым",
+  },
+  {
+    id: "41",
+    engText: "I don't have any brothers or sisters",
+    rusText: "У меня нет каких-либо братьев или сестер",
+  },
+  {
+    id: "4117",
+    engText: "I don't have any questions",
+    rusText: "У меня нет каких-либо вопросов",
+  },
+  {
+    id: "2064",
+    engText: "I don't think that it really works",
+    rusText: "Я не думаю, что это реально работает",
+  },
+  {
+    id: "3378",
+    engText: "He doesn't think that it really works",
+    rusText: "Он не думает, что это действительно работает",
+  },
+  {
+    id: "3598",
+    engText: "He doesn't like this food",
+    rusText: "Ему не нравится эта еда",
+  },
+  {
+    id: "2002",
+    engText: "She doesn't want to go there now",
+    rusText: "Она не хочет идти туда сейчас",
+  },
+  {
+    id: "4975",
+    engText: "I don't see any changes",
+    rusText: "Я не вижу каких-либо изменений",
+  },
+  {
+    id: "7991",
+    engText: "He doesn't want to say it",
+    rusText: "Он не хочет говорить это",
+  },
+  {
+    id: "9931",
+    engText: "I don't want to talk about it",
+    rusText: "Я не хочу говорить об этом",
+  },
+  {
+    id: "1735",
+    engText: "I don't have time",
+    rusText: "У меня нет времени",
+  },
+  {
+    id: "2018",
+    engText: "He doesn't have money",
+    rusText: "У него нет денег",
+  },
+  {
+    id: "8116",
+    engText: "He doesn't have this money",
+    rusText: "У него нет этих денег",
+  },
+  {
+    id: "5670",
+    engText: "I know that it doesn't cos this money",
+    rusText: "Я знаю, что это не стоит этих денег",
+  },
+  {
+    id: "6099",
+    engText: "I don't want to stop. I want to continue",
+    rusText: "Я не хочу остановиться. Я хочу продолжить",
+  },
+];
---
+++ src/app/Lessons/LessonA0/LessonA0-7/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Link from "next/link";
+import { rusEngText7 } from "./RusEngText7.js";
+export default function Home() {
+  const [blurEng, setBlurEng] = useState(true);
+  const [blurRus, setBlurRus] = useState(false);
+  const [blurFull, setBlurFull] = useState(true);
+  useEffect(() => {
+    AOS.init({
+      duration: 700,
+    });
+  }, []);
+  return (
+    <div className="">
+      <div className="inline-block">
+        <div className="flex xl:justify-between md:justify-center  ms-5 justify-start w-[160px] sm:flex-row flex-col gap-5 lg:gap-0 sm:w-[900px] sm:ms-10 mt-20 me-10">
+          <Link
+            href="/Lessons/LessonA0/LessonA0-6"
+            as={"/Lessons/LessonA0/LessonA0-6"}
+            className="px-5 py-2 rounded-xl border-2 border-slate-500"
+          >
+            ← LessonA0-6
+          </Link>
+          <Link
+            href="/Lessons/LessonA0/LessonA0-8"
+            as={"/Lessons/LessonA0/LessonA0-8"}
+            className="px-5 py-2 rounded-xl border-2 border-slate-500"
+          >
+            LessonA0-8 →
+          </Link>
+        </div>
+      </div>
+      <div className="flex gap-8  place-content-center xl:text-[20px] items-start lg:items-center lg:text-[18px] flex-col lg:flex-row border-b-2 border-white pb-10 mt-10 sm:ms-10 ms-5 w-[700px] lg:w-[900px]">
+        <div className="text-[20px] sm:ps-2 lg:text-[40px]">A0 - lesson 7</div>
+        <button
+          onClick={() => setBlurEng(!blurEng)}
+          className="px-5 py-3 border-2 border-slate-500 rounded-xl font-['Inter']"
+        >
+          Blur Eng.
+        </button>
+        <button
+          onClick={() => setBlurRus(!blurRus)}
+          className="px-5 py-3 border-2 border-slate-500 rounded-xl font-['Inter']"
+        >
+          Blur Rus.
+        </button>
+        <button
+          onClick={() => setBlurFull(!blurFull)}
+          className="px-5 py-3 border-2 border-slate-500 rounded-xl font-['Inter']"
+        >
+          Blur Full
+        </button>
+      </div>
+      <div className="xl:text-[25px] lg:text-[20px]  mt-10 sm:ms-10 mb-20 ms-5 flex flex-col gap-10 lg:w-[900px] w-[700px] font-['Inter']">
+        {rusEngText7.map((item) => (
+          <div key={item.engText} className="">
+            <div
+              data-aos="fade-right"
+              className="flex gap-10 border-b-2 dark:border-white border-black  pb-10"
+            >
+              <div className="flex flex-col gap-5  border-r-4 w-[1000px] dark:border-white border-black">
+                <div
+                  className={`flex-col flex sm:flex-row transition duration-500 ${
+                    blurFull ? "blur-[0px]" : "blur-[10px] hover:blur-[0px]"
+                  }`}
+                >
+                  <div
+                    className={`px-2 transition duration-500 lg:px-5 ${
+                      blurEng ? "blur-[0px]" : "blur-[10px] hover:blur-[0px]"
+                    }`}
+                  >
+                    {item.engText}
+                  </div>
+                  <div className="lg:px-0 px-2">-</div>
+                  <div
+                    className={` px-2 transition duration-500 lg:px-5 ${
+                      blurRus ? "blur-[0px]" : "blur-[10px] hover:blur-[0px]"
+                    }`}
+                  >
+                    {item.rusText}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
